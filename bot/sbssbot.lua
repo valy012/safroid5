2dd5ebe2d9c9752ede6c899678e319d4ba2364b0
valy012 <valy.ghorbani@gmail.com>
2016-10-14 02:55:32 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {199642007,29387273},--Sudo users
+    sudo_users = {199642007,298387273},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
